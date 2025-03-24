c829712cee7d2e1e6ad9fb9fd8b5e7d7e042a16c
ACOSTA4RL <acostarjayluis54@gmail.com>
2025-03-24 09:26:05 +0800

    Update FavNumCalculation4.js

--- DSA_Midterm/FavNumCalculation4.js
+++ DSA_Midterm/FavNumCalculation4.js
@@ -6,7 +6,7 @@ const theFavNumber = 7;
 let guess; //This declares a variable 'guess' that will store the user's input.
 
 while (guess !== theFavNumber) { //The loop runs until the user guesses the correct number.
-    guess = parseInt(prompt("Guess my favorite number: "), 10);
+    guess = Number(prompt("Guess my favorite number: "), 10);
     //Prompts the user for input and converts it to an integer.
 
     if (guess > theFavNumber) { //Checks if the guessed number is greater than the favorite number.
